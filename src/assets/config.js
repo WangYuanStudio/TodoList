acb53967a8e52414978f82eadf0378c432f0d1c9
kooritea <ochatokori@gmail.com>
2018-06-18 23:00:48 +0800

    build:0618 22:57:06

--- src/assets/config.js
+++ src/assets/config.js
@@ -2,6 +2,6 @@ const config={
   //URLBASE:'http://frp.yuanmoc.com:7002/index.html/#/personal',
   URLBASE:'http://temp.yuanmoc.com/dist/index.html#personal',
   APIBASE:'http://test.yuanmoc.com',
-  build:'0618 22:51:06'
+  build:'0618 22:57:06'
 }
 export default config
